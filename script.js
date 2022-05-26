6133c7c16509d844aba9e2ccdf36066f1be15a21
Karyna-Ko <karina.korshak@gmail.com>
2022-05-26 11:54:39 +0300

    added functions

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-/* Задание (УРОК 1):
+ 'use strict';
+let numberOfFilms;
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+function start() {
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
+    while (numberOfFilms == '' || numberOfFilms  == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
     }
- */ 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -28,40 +17,48 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-/* const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d; */
+ function rememberMyFilms() {
+    for (let i = 0; i <2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log("done!"); 
+        } else  {
+            console.log('error!');
+            i--;
+        }
+    }
+ }
+ rememberMyFilms();
 
-// Задание (УРОК 2):
- // 1. Автоматизировать вопросы пользователю про фильмы при помощи цикла. 
+ function detectPersonalLevel () {
+    if (personalMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов"); 
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классный зритель"); 
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман"); 
+    } else {
+        console.log("Произошла ошибка");
+    }
+ }
+ detectPersonalLevel ();
 
-for (let i = 0; i <2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log("done!"); 
-    } else  {
-        console.log('error!');
-        i--;
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
     }
 }
+showMyDB (personalMovieDB.privat);
 
-if (personalMovieDB.count < 10){
-    console.log("Просмотрено довольно мало фильмов"); 
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классный зритель"); 
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман"); 
-} else {
-    console.log("Произошла ошибка");
+function writeYourGenres () {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
 }
- // 2. Сделать так, чтобы пользователь не смог оставить ответ в виде пустой строки, отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как str.lenght - и получить её длину).
+writeYourGenres ();
 
- // 3. При помощи условий проверить personalMovieDB.count, и если он меньше 10, вывести сообщение: "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классный зритель", а если больше - "Вы киноман". А если не подошло ни одному варианту - "Произошла ошибка".
 
